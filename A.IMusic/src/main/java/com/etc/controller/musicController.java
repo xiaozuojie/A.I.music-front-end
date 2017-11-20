118aa0c7a3860d880180af1dc669b2593d083ad1
xiangrian <xiangrian@DESKTOP-25QN02G>
2017-11-20 12:18:45 +0800

    整合登录，注册页面

--- A.IMusic/src/main/java/com/etc/controller/musicController.java
+++ A.IMusic/src/main/java/com/etc/controller/musicController.java
@@ -1,10 +1,16 @@
 package com.etc.controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.etc.entity.Albums;
 import com.etc.entity.Playlist;
 import com.etc.entity.Song;
